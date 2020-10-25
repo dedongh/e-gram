b6c6de916a8ee839bb06901302f9d733ff934493
braemma <asiamahemma95@gmail.com>
2020-10-25 00:42:35 +0000

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCWNl2BdwQbBkq8Pvj1s_n0ecMbALoJu7U',
+    authDomain: 'kasa-7da25.firebaseapp.com',
+    databaseURL: 'https://kasa-7da25.firebaseio.com',
+    projectId: 'kasa-7da25',
+    storageBucket: 'kasa-7da25.appspot.com',
+    messagingSenderId: '756276944787',
+    appId: '1:756276944787:web:c85cfc733040a9d1045266'
+}
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// collection references
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+// export utils/refs
+export {
+    db,
+    auth,
+    usersCollection,
+    postsCollection,
+    commentsCollection,
+    likesCollection
+}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {auth} from  './firebase'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// ensure firebase initializes before loading
+// the app when a user refreshes a page
+let app
+auth.onAuthStateChanged(() =>{
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
